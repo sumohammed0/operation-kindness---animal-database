778ff7be7d35053ac11541883ef25fa5f18cd4b6
Safa Mohammed <sumohammed0@gmail.com>
2022-04-20 09:50:46 -0500

    Initial Commit

---
+++ HW 6 - operation kindness/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW 6 - operation kindness
+//
+//  Created by Safa Mohammed on 4/20/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
